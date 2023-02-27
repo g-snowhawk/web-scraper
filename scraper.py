fa460a1e6cdc730f4bce2c0d9720e2e9b99b3ea8
g-snowhawk <16555946+g-snowhawk@users.noreply.github.com>
2023-02-27 16:16:16 +0900

    Add support interval

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import html5lib
+import html5lib # noqa
 import os
 import re
 import requests
@@ -10,9 +10,15 @@ import sys
 
 from bs4 import BeautifulSoup
 from pathlib import Path
+from random import randint
+from requests_html import HTMLSession, AsyncHTMLSession
+from time import sleep
 from urllib.parse import urlparse, urljoin
 
-from requests_html import HTMLSession, AsyncHTMLSession
+
+class HttpError(Exception):
+    pass
+
 
 def savefile(url, src):
     global args
@@ -21,11 +27,54 @@ def savefile(url, src):
 
     path = f"{args.mirror}{uri.path}"
 
-    directory = os.path.dirname(path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    try:
+        directory = os.path.dirname(path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-    Path(path).write_bytes(src)
+        Path(path).write_bytes(src)
+    except PermissionError as e:
+        print(e, file=sys.stderr)
+    except IsADirectoryError as e:
+        print(e, file=sys.stderr)
+
+
+def browse(url):
+    global args, session
+
+    r = (requests.get(url, stream=True)
+         if args.noscript is True
+         else session.get(url, stream=True))
+    if r.status_code == 200:
+        if r.headers['content-type'] == 'text/css':
+            m = re.findall(r'url\([\'"]?(.+?)[\'"]?\)', r.text)
+            for path in m:
+                u = urlparse(urljoin(url, path))
+
+                if u.netloc != '' and u.netloc != base_url.netloc:
+                    continue
+
+                ur = u.geturl()
+
+                if ur in cache_url:
+                    continue
+
+                cache_url.append(ur)
+
+                try:
+                    browse(ur)
+                except HttpError as e:
+                    print(e, file=sys.stderr)
+
+        if args.mirror != '':
+            savefile(url, r.content)
+
+    elif r.status_code >= 400:
+        raise HttpError(str(r.status_code) + ': ' + url)
+
+    if args.progress is True:
+        print(f"{url} ({r.status_code})")
+
 
 def get_elements(soup, parser, tag, attr):
     global args, base_url, cache_url, session
@@ -39,57 +88,36 @@ def get_elements(soup, parser, tag, attr):
         if (uri.path == '' and uri.query == ''):
             continue
 
-        if (re.match('^\./', uri.path)):
-            uri = uri._replace(path=re.sub('^\./', '', uri.path))
+        if (re.match(r'^\./', uri.path)):
+            uri = uri._replace(path=re.sub(r'^\./', '', uri.path))
 
         if uri.path == '':
             uri = uri._replace(path='/')
-        elif re.match('^\.\./', uri.path) is not None:
+        else:
             uri = urlparse(urljoin(parser.geturl(), uri.path))
 
-        url = base_url._replace(path=uri.path, query=uri.query, fragment=uri.fragment).geturl()
-        url = re.sub('/+\.?$', '/', url)
+        url = base_url._replace(path=uri.path,
+                                query=uri.query,
+                                fragment=uri.fragment).geturl()
+        url = re.sub(r'/+\.?$', '/', url)
 
         if url in cache_url:
             continue
 
         cache_url.append(url)
 
-        r = requests.get(url, stream=True) if args.noscript == True else session.get(url, stream=True)
-        if r.status_code == 200:
-            if r.headers['content-type'] == 'text/css':
-                m = re.findall('url\([\'"]?(.+?)[\'"]?\)', r.text)
-                for path in m:
-                    u = urlparse(urljoin(url, path))
+        try:
+            browse(url)
+        except HttpError as e:
+            print(e, file=sys.stderr)
 
-                    if u.netloc != '' and u.netloc != base_url.netloc:
-                        continue
-
-                    ur = u.geturl()
-
-                    if ur in cache_url:
-                        continue
-
-                    cache_url.append(ur)
-                    ri = requests.get(ur, stream=True) if args.noscript == True else session.get(ur, stream=True)
-                    if ri.status_code == 200:
-                        if args.mirror != '':
-                            savefile(ur, ri.content)
-
-                    if args.progress == True:
-                        print(f"{ur} ({ri.status_code})")
-
-            if args.mirror != '':
-                savefile(url, r.content)
-
-        if args.progress == True:
-            print(f"{url} ({r.status_code})")
 
 async def rendering(url):
     global session
     r = await session.get(url)
     await r.html.arender()
     return r
+
 
 def crawl(url):
     global args, base_url, cache_url, session
@@ -102,28 +130,33 @@ def crawl(url):
     r = None
     source = ''
 
-    if args.noscript == True:
-        r = requests.get(url)
-        r.encoding = r.apparent_encoding
-        source = r.text
-    else:
-        if args.asyncr != True:
-            r = session.get(url)
-            r.html.render()
+    try:
+        if args.noscript is True:
+            r = requests.get(url)
+            r.encoding = r.apparent_encoding
+            source = r.text
         else:
-            r = asession.run(lambda url=url: rendering(url))[0]
-        r.encoding = r.apparent_encoding
-        source = r.html.html
+            if args.asyncr is not True:
+                r = session.get(url)
+                r.html.render()
+            else:
+                r = session.run(lambda url=url: rendering(url))[0]
+            r.encoding = r.apparent_encoding
+            source = r.html.html
+    except Exception as e:
+        print(e, file=sys.stderr)
+        return
 
     if r.status_code >= 400:
-        print(str(r.status_code) + ':' + url)
-        return
+        raise HttpError(str(r.status_code) + ': ' + url)
 
     # detect default index
     if re.search('/$', url) is not None:
         for index in ['index.html', 'index.htm']:
             idx = url + index
-            res = requests.head(idx, allow_redirects=False) if args.noscript == True else session.head(idx, allow_redirects=False)
+            res = (requests.head(idx, allow_redirects=False)
+                   if args.noscript is True
+                   else session.head(idx, allow_redirects=False))
             if res.status_code == 200 or res.status_code == 302:
                 url = idx
                 break
@@ -132,7 +165,7 @@ def crawl(url):
 
     tmp = urlparse(url)
 
-    if args.progress == True:
+    if args.progress is True:
         print(f"{tmp.geturl()} ({r.status_code})")
 
     # get parts
@@ -144,7 +177,7 @@ def crawl(url):
         savefile(url, r.content)
 
     # recurse
-    if args.recursive == True:
+    if args.recursive is True:
         for a in soup.find_all('a'):
             uri = urlparse(a.get('href'))
 
@@ -157,36 +190,56 @@ def crawl(url):
             if (uri.path == '' and uri.query == ''):
                 continue
 
-            if (re.match('^\./', uri.path)):
-                uri = uri._replace(path=re.sub('^\./', '', uri.path))
+            if (re.match(r'^\./', uri.path)):
+                uri = uri._replace(path=re.sub(r'^\./', '', uri.path))
 
             if uri.path == '':
                 uri = uri._replace(path='/')
-            elif re.match('^\.\./', uri.path) is not None:
+            else:
                 uri = urlparse(urljoin(tmp.geturl(), uri.path))
 
-            url = base_url._replace(path=uri.path, query=uri.query, fragment=uri.fragment).geturl()
-            url = re.sub('/+\.?$', '/', url)
+            url = base_url._replace(path=uri.path,
+                                    query=uri.query,
+                                    fragment=uri.fragment).geturl()
+            url = re.sub(r'/+\.?$', '/', url)
 
-            crawl(url)
+            try:
+                crawl(url)
+                interval = randint(0, args.interval)
+                if interval > 0:
+                    sleep(interval)
+            except HttpError as e:
+                print(e, file=sys.stderr)
+
 
 def main():
     global args, base_url, cache_url, session
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--async', action='store_true', dest='asyncr', help='Async crawling')
-    parser.add_argument('-m', '--mirror', nargs='?', default='', help='Mirroring path')
-    parser.add_argument('-n', '--noscript', action='store_true', help='Noscript')
-    parser.add_argument('-p', '--progress', action='store_true', help='Progress crawling')
-    parser.add_argument('-r', '--recursive', action='store_true', help='Crawl recursive')
-    parser.add_argument('url', nargs='?', help='URL')
-    args = parser.parse_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('-a', '--async', action='store_true', dest='asyncr',
+                   help='Async crawling')
+    p.add_argument('-i', '--interval', nargs='?', default=0, type=int,
+                   help='Crawl interval')
+    p.add_argument('-m', '--mirror', nargs='?', default='',
+                   help='Mirroring path')
+    p.add_argument('-n', '--noscript', action='store_true',
+                   help='Noscript')
+    p.add_argument('-p', '--progress', action='store_true',
+                   help='Progress crawling')
+    p.add_argument('-r', '--recursive', action='store_true',
+                   help='Crawl recursive')
+    p.add_argument('url', nargs='?', help='URL')
+    args = p.parse_args()
 
     if args.mirror != '':
-        args.mirror = os.path.expanduser(args.mirror) if re.match('~', args.mirror) else os.path.abspath(args.mirror)
+        args.mirror = (os.path.expanduser(args.mirror)
+                       if re.match('~', args.mirror)
+                       else os.path.abspath(args.mirror))
 
-    if args.noscript != True:
-        session = HTMLSession() if args.asyncr != True else AsyncHTMLSession()
+    if args.noscript is not True:
+        session = (HTMLSession()
+                   if args.asyncr is not True
+                   else AsyncHTMLSession())
 
     base_url = urlparse(args.url)
 
@@ -200,8 +253,10 @@ def main():
 
     if args.mirror == '':
         cache_url.sort()
+        print('-- Crawled -------------------------------------')
         for url in cache_url:
             print(url)
+
 
 if __name__ == '__main__':
     args = None
